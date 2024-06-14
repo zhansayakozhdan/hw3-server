49c0d0bb379ae7b1a49c24ab3ac47e4b2be283b6
Zhansaya Kozhdan <zhkozhdan09@gmail.com>
2024-06-14 05:29:48 +0300

    c

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://hw3-client.vercel.app/",
     methods: ["GET", "POST"]
   }
 });
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'https://hw3-client.vercel.app/'
 }));
 
 app.use('/api', adsRouter);
